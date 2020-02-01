9170da6f13e7253104140db711ce49a0e7efa869
Pommanthebrit <warburmf@hotmail.com>
2020-02-01 13:09:10 +0800

    cookie clicker temp

---
+++ Assets/Myles Scripts/AutoClicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoClicker : MonoBehaviour
+{
+    public bool creatingcookie = false;
+
+
+    void Update()
+    {
+        if(creatingcookie == false)
+        {
+            StartCoroutine(createcookiesAuto());
+            creatingcookie = true;
+        }
+    }
+
+    IEnumerator createcookiesAuto()
+    {
+        BasicCokieFunctintion.CookieCount += 1;
+        yield return new WaitForSeconds(1);
+        creatingcookie = false;
+    }
+
+   
+}
---
+++ Assets/Myles Scripts/BasicCokieFunctintion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicCokieFunctintion : MonoBehaviour
+{
+    public static int CookieCount;
+    public GameObject CookieDisplay;
+    public int internalCookie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalCookie = CookieCount;
+        CookieDisplay.GetComponent<Text>().text = "Cookies " + internalCookie;
+    }
+}
