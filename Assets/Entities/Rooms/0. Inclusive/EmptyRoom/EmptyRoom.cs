0d510feea6b768121ce0221a1b5d98cf84005dae
Greysion (Hayden Reeve) <haydenreeve@hotmail.com>
2020-02-02 20:09:51 +0800

    Room Prefabs, Configs, Templates, and Sprites for Mainframe
    - Sprites added for Mainframe room
    - Room Prefabs and Structure setup with Builders/Configs/Backgrounds
    - Templates set up for Mainframe
    - Templates altered to adjust to EmptyRoom.
    
    Co-Authored-By: luccubus <luccubus@users.noreply.github.com>

--- Assets/Entities/Rooms/0. Inclusive/EmptyRoom/EmptyRoom.cs
+++ Assets/Entities/Rooms/0. Inclusive/EmptyRoom/EmptyRoom.cs
@@ -19,5 +19,4 @@ public class EmptyRoom : Room
 		return;
 	}
 
-
 }
---
+++ Assets/Entities/Rooms/ComponentData.cs
@@ -0,0 +1,19 @@
+﻿/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
+   Author: 			Hayden Reeve
+   Description: 	Sole purpose to make instantiation easier by containing preset references to 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+using UnityEngine;
+
+public class ComponentData : MonoBehaviour
+{
+
+    [SerializeField] public SpriteRenderer myBackground;
+    [SerializeField] public SpriteRenderer myForeground;
+    [SerializeField] public SpriteRenderer myShadow;
+
+    public void Finished() {
+        Destroy(this);
+    }
+
+}
